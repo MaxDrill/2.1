b917de2ddac91ce551b3fbbd20f6bed4622a62ad
MaxDrill <konyjeff@yandex.ru>
2022-11-21 21:49:33 +0300

    add of folders

---
+++ proj/arithmetic.py
@@ -0,0 +1,4 @@
+sol = input("what is the answer?\n4*100-54 = ")
+corsol = 4*100-54
+print ("the correct solution is: ",corsol)
+print ("your answer is: ",sol)
---
+++ proj/numbers.py
@@ -0,0 +1,12 @@
+print("enter four numbers: ")
+
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+num4 = int(input())
+
+sum1 = num1 + num2
+sum2 = num3 + num4
+div = sum1 / sum2
+
+print("Otvet: ",format(div,".2"))
