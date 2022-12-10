f4f6ca848a235cf615b7176bbc7b5cbfc1632b53
KonyzhevMax <112752533+MaxDrill@users.noreply.github.com>
2022-12-10 11:37:24 +0300

    Update prog

--- proj/individual.py
+++ proj/individual.py
@@ -3,4 +3,4 @@ from math import sqrt
 a = float(input("Длина большего основания:"))
 b = float(input("Длина меньшего основания:"))
 h = float(input("Высота:"))
-print("P =", a + b + 2 * sqrt(sqrt(h) + sqrt(a - b) / 4))
+print("P =", a + b + 2 * sqrt(pow(h, 2) + pow((a - b) / 2, 2)))
